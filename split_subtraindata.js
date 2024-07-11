4d46d76a79a9dd31737d8acd418ca5a888444a24
KNU-K <knukang334@gmail.com>
2024-07-11 17:41:12 +0900

    add : sub train data 로 분리하는 작업 진행

--- split_subtraindata.js
+++ split_subtraindata.js
@@ -1,9 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const filePath = path.join(__dirname, "result.json"); // result.json 파일 경로 설정
-
-// 파일 읽기
+const filePath = path.join(__dirname, "./train_data/result.json");
 fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
         console.error("Error reading file:", err);
@@ -11,38 +9,26 @@ fs.readFile(filePath, "utf8", (err, data) => {
     }
 
     try {
-        const jsonData = JSON.parse(data); // JSON 문자열을 JavaScript 객체로 변환
-
-        let allItems = []; // 모든 요소를 저장할 배열
-
-        // 최상위 배열을 순회하면서 요소들을 모두 모아 저장
+        const jsonData = JSON.parse(data);
+        let allItems = [];
         for (const item of jsonData) {
             if (Array.isArray(item)) {
-                // 내부 배열이 있는 경우
-                allItems = allItems.concat(item); // 내부 배열의 요소들을 모두 추가
+                allItems = allItems.concat(item);
             } else {
-                // 내부 배열이 없는 경우
-                allItems.push(item); // 요소를 바로 추가
+                allItems.push(item);
             }
         }
 
-        // 서브 JSON 파일 생성
-        const batchSize = 1000; // 각 파일에 저장할 데이터의 개수
-        const numBatches = Math.ceil(allItems.length / batchSize); // 필요한 서브 JSON 파일의 개수
-
+        const batchSize = 100;
+        const numBatches = Math.ceil(allItems.length / batchSize);
         for (let i = 0; i < numBatches; i++) {
             const startIndex = i * batchSize;
             const endIndex = Math.min(startIndex + batchSize, allItems.length);
-            const batch = allItems.slice(startIndex, endIndex); // batchSize 만큼 데이터를 자름
-
-            const subJsonData = JSON.stringify(batch, null, 2); // JSON 문자열로 변환
-
-            const filename = `subresult_${i + 1}.json`; // 서브 JSON 파일 이름 설정
-            const subFilePath = path.join(__dirname, filename); // 서브 JSON 파일 경로 설정
-
-            // 파일에 데이터 쓰기
+            const batch = allItems.slice(startIndex, endIndex);
+            const subJsonData = JSON.stringify(batch, null, 2);
+            const filename = `./train_data/subresult_${i + 1}.json`;
+            const subFilePath = path.join(__dirname, filename);
             fs.writeFileSync(subFilePath, subJsonData, "utf8");
-
             console.log(`Subfile ${filename} has been saved.`);
         }
     } catch (parseError) {
